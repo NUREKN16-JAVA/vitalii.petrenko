326bfc20936c63933d4dacf90fd41d7af497ba7d
Vitaliy Petrenko <vesperitine@gmail.com>
2018-12-26 05:05:26 +0200

    fix old files

--- usermanagement/src/main/java/ua/nure/petrenko_vitaliy/usermanagement/database/ConnectionFactory.java
+++
@@ -1,7 +0,0 @@
-package main.java.ua.nure.kn.petrenko_vitaliy.usermanagment.database;
-
-import java.sql.Connection;
-
-public interface ConnectionFactory {
-	Connection createConnection() throws DatabaseCustomException;
-}
